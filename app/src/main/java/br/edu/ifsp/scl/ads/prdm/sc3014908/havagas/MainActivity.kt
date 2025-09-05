3fbf2f67c5d16367585c6f1e6829fc9c4d260742
Vitor Aguiar <galaxiustraveler@outlook.com>
2025-09-05 15:19:20 -0300

    feat: adiciona layout

--- app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3014908/havagas/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3014908/havagas/MainActivity.kt
@@ -1,20 +1,19 @@
 package br.edu.ifsp.scl.ads.prdm.sc3014908.havagas
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import br.edu.ifsp.scl.ads.prdm.sc3014908.havagas.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var activityMainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+    }
+
+    fun teste() {
+
     }
 }
